8ce7f2648c4207b410c29678830c6f27f6a19596
Can335 <canlahu1@gmail.com>
2024-09-02 11:52:24 +0200

    Added some things like slots script profilewrapper written and yeh

---
+++ src/ReplicatedStorage/modules/ProfileServiceWrapper.lua
@@ -0,0 +1,61 @@
+local ProfileService = require(script.Parent.ProfileService)
+
+local ProfileWrapper = {}
+
+ProfileWrapper.__index = ProfileWrapper
+
+function ProfileWrapper.New(Key:string, Template : {[any] : any})
+    
+local self = setmetatable({}, ProfileWrapper)
+
+self.Profile = ProfileService.GetProfileStore(Key, Template)
+
+self.Profiles = {} :: {[string]: any}
+
+return self
+end
+
+function ProfileWrapper.NewProfile(self : {}, Player)
+    
+if Player:IsDescendantOf(game.Players) then
+
+local Profile = self.Profile:LoadProfileAsync(Player.Name .. "_" .. Player.UserId)
+
+if Profile then
+    
+self.Profiles[Player.Name .. Player.UserId] = Profile
+
+                            end
+
+else
+Profile:Release()
+if self.Profiles[Player.Name .. Player.UserId] then
+    
+self.Profiles[Player.Name .. Player.UserId] = nil
+Player:Kick("Data Problems")
+
+                    end
+
+Player:Kick("Data Problems")
+
+        end
+
+end
+
+
+
+function ProfileWrapper.ReleaseProfile(self : {}, Player)
+    
+if self.Profiles[Player.Name .. Player.UserId] then
+
+self.Profiles[Player.Name .. Player.UserId]:Release()
+
+self.Profiles[Player.Name .. Player.UserId] = nil
+
+    end
+
+end
+
+
+
+return ProfileWrapper
